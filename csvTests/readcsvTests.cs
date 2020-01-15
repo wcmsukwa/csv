6aff4dd144d3bf4995d87de4bc3098d2944b08a5
Keith Williams <keith@webizly.com>
2020-01-15 13:20:42 +0200

    Feature: Added Stream Writer of CSV File

--- csvTests/readcsvTests.cs
+++ csvTests/readcsvTests.cs
@@ -1,9 +1,11 @@
 ﻿using Cities;
 using Csv;
+using CsvHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace csv.Tests
@@ -17,16 +19,24 @@ namespace csv.Tests
             var path = "c://csvfiles//worldcities.csv";
             var doubleTypeConversion = new DoubleConversion();
             IList<CityModel> myList = ReadCsv.ReadCsvFile<CityModel, CityMap>(path, doubleTypeConversion);
-            var cityList = from s in myList
+            var countryCapitalQuery = from s in myList
                            where s.Capital.Equals("primary")
                            orderby s.Country ascending
                            select s;
-            foreach (CityModel city in cityList)
+            foreach (CityModel city in countryCapitalQuery)
             {
                 Debug.Write(city.Country + ": " + city.City_name + Environment.NewLine);
             }
+            var queryName = nameof(countryCapitalQuery);
+            var writePath = "c://csvfiles//" +  queryName  + ".csv";
+            using (var writer = new StreamWriter(writePath))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(countryCapitalQuery);
+            }
+            Assert.IsTrue(File.Exists(writePath));
 
-            var QSCount = (from city in cityList
+            var QSCount = (from city in countryCapitalQuery
                            select city).Count();
 
             Debug.Write(QSCount);
