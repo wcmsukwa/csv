d90b23c5b6fd9bd08974f0421c458285c20b6e2d
Keith Williams <keith@webizly.com>
2020-01-15 12:31:57 +0200

    Refactor:  Removed Type Map and Made it return the correct list type.

--- csv/readcsv.cs
+++ csv/readcsv.cs
@@ -1,39 +1,28 @@
 ﻿using CsvHelper;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Csv
 {
     public class ReadCsv
     {
-        public static IList ReadCsvFile<T>(string absolutePath, object Model, object ModelMap, CsvHelper.TypeConversion.ITypeConverter DoubleTypeConversion)
+        public static IList<T> ReadCsvFile<T, M>(string absolutePath, CsvHelper.TypeConversion.ITypeConverter DoubleTypeConversion)
         {
-            Type objectType = Model.GetType();
-            Type t = typeof(T);
-
-            var listType = typeof(List<>).MakeGenericType(t);
-            var results = (IList)Activator.CreateInstance(listType);
-            //dynamic results = new List<T>();
+            Type mapType = typeof(M);
+            IList<T> results = new List<T>();
             using var reader = new StreamReader(absolutePath);
             using (var csv = new CsvReader(reader))
             {
                 csv.Configuration.HasHeaderRecord = true;
                 csv.Configuration.TypeConverterCache.AddConverter(typeof(double), DoubleTypeConversion);
-                Type mapType = ModelMap.GetType();
                 csv.Configuration.RegisterClassMap(mapType);
-                
+
                 while (csv.Read())
                 {
-
-                    // var instantiatedObject = Activator.CreateInstance(objectType);
-                    results.Add(csv.GetRecord(objectType));
+                    results.Add(csv.GetRecord<T>());
                 }
-                
             }
-            // queryResult = results.AsQueryable();
             return results;
         }
     }
--- csvTests/readcsvTests.cs
+++ csvTests/readcsvTests.cs
@@ -3,40 +3,35 @@ using Csv;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Diagnostics;
+using System.Linq;
 
 namespace csv.Tests
 {
     [TestClass()]
-    public class readcsvTests
+    public class ReadcsvTests
     {
         [TestMethod()]
         public void ReadInCSVTest()
         {
             var path = "c://csvfiles//worldcities.csv";
-            var model = new CityModel();
-            var map = new CityMap();
             var doubleTypeConversion = new DoubleConversion();
-            IList<CityModel> myList = (IList<CityModel>)ReadCsv.ReadCsvFile<CityModel>(path, model, map, doubleTypeConversion);
-            //var cityList = myList.Where(city => city.Capital.Equals("primary"));
+            IList<CityModel> myList = ReadCsv.ReadCsvFile<CityModel, CityMap>(path, doubleTypeConversion);
             var cityList = from s in myList
                            where s.Capital.Equals("primary")
                            orderby s.Country ascending
                            select s;
-            foreach (CityModel city in cityList) {
+            foreach (CityModel city in cityList)
+            {
                 Debug.Write(city.Country + ": " + city.City_name + Environment.NewLine);
             }
 
             var QSCount = (from city in cityList
                            select city).Count();
 
-
             Debug.Write(QSCount);
-            
-            Assert.AreEqual(15493, myList.Count());
-            
 
+            Assert.AreEqual(15493, myList.Count());
 
             // countryQuery = records.Where(city => city.Country.Equals("United States"));
             /*
